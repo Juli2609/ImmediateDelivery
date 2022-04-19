096a52e87d9b687c9322508ac58c27c4628e8877
JULIANA ARROYAVE ARANGO <julianaarroyave301009@correo.itm.edu.co>
2022-04-19 15:17:39 -0500

    Create ComboHelper

---
+++ ImmediateDelivery/ImmediateDelivery/Helpers/CombosHelper.cs
@@ -0,0 +1,61 @@
+﻿using ImmediateDelivery.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImmediateDelivery.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _context;
+
+        public CombosHelper(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<SelectListItem>> GetComboAddressesAsync(int neighborhoodId)
+        {
+            List<SelectListItem> list = await _context.Addresses
+                .Where(s => s.Neighborhood.Id == neighborhoodId)
+                .Select(c => new SelectListItem
+                {
+                    Text = c.Name,
+                    Value = c.Id.ToString()
+                })
+                .OrderBy(c => c.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem { Text = "[Seleccione una dirección...", Value = "0" });
+            return list;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetComboCitiesAsync()
+        {
+            List<SelectListItem> list = await _context.Cities.Select(c => new SelectListItem
+            {
+                Text = c.Name,
+                Value = c.Id.ToString()
+            })
+              .OrderBy(c => c.Text)
+              .ToListAsync();
+
+            list.Insert(0, new SelectListItem { Text = "[Seleccione una ciudad...", Value = "0" });
+            return list;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetComboNeighborhodsAsync(int cityId)
+        {
+            List<SelectListItem> list = await _context.Neighborhoods
+                .Where(s => s.City.Id == cityId)
+                .Select(c => new SelectListItem
+                {
+                    Text = c.Name,
+                    Value = c.Id.ToString()
+                })
+                .OrderBy(c => c.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem { Text = "[Seleccione un Barrio/Vereda...", Value = "0" });
+            return list;
+        }
+    }
+}
---
+++ ImmediateDelivery/ImmediateDelivery/Helpers/ICombosHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ImmediateDelivery.Helpers
+{
+    public interface ICombosHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetComboCitiesAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboNeighborhodsAsync(int countryId);
+
+        Task<IEnumerable<SelectListItem>> GetComboAddressesAsync(int stateId);
+
+    }
+}
